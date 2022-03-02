5f2f1535c81a0f4c3a8bd06d410ce46782d46db5
salmamoh905 <samiramoh246@gmail.com>
2022-03-02 17:40:26 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+
+function App() {
+  return (
+    <div >
+      <Header/>
+      <div className="container">
+       <Balance/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/IncomeExpenses.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const IncomeExpenses = () => {
+  return (
+    <div>IncomeExpenses</div>
+  )
+}
